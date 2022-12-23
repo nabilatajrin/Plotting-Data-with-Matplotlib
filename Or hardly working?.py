b0bdd2f34af50187d5f7ec017c88f81b77a30301
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-12-23 16:23:42 +0600

    Update Or hardly working?.py

--- Or hardly working?.py
+++ Or hardly working?.py
@@ -2,10 +2,10 @@
 plt.plot(deshaun.day_of_week, deshaun.hours_worked)
 
 # Plot Officer Aditya's hours_worked vs. day_of_week
-plt.plot(mengfei.day_of_week, mengfei.hours_worked)
+plt.plot(aditya.day_of_week, aditya.hours_worked)
 
 # Plot Officer Mengfei's hours_worked vs. day_of_week
-plt.plot(aditya.day_of_week, aditya.hours_worked)
+plt.plot(mengfei.day_of_week, mengfei.hours_worked)
 
 # Display all three line plots
 plt.show()
