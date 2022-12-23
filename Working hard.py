6431bc217e7df892839c11da5e24a22608a01814
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-12-23 16:14:07 +0600

    Update Working hard.py

--- Working hard.py
+++ Working hard.py
@@ -3,3 +3,6 @@ from matplotlib import pyplot as plt
 
 # Plot Officer Deshaun's hours_worked vs. day_of_week
 plt.plot(deshaun.day_of_week, deshaun.hours_worked)
+
+# Display Deshaun's plot
+plt.show()
