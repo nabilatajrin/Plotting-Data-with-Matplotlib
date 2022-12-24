502d796a2f34d07fa73dd9300334ae1f6ee89b5f
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-12-24 13:45:17 +0600

    Create Playing with styles.py

---
+++ Playing with styles.py
@@ -0,0 +1,15 @@
+# Change the style to fivethirtyeight
+plt.style.use('fivethirtyeight')
+
+# Plot lines
+plt.plot(data["Year"], data["Phoenix Police Dept"], label="Phoenix")
+plt.plot(data["Year"], data["Los Angeles Police Dept"], label="Los Angeles")
+plt.plot(data["Year"], data["Philadelphia Police Dept"], label="Philadelphia")
+
+# Add a legend
+plt.legend('vvv')
+
+# Display the plot
+plt.show()
+
+print(plt.style.available)
