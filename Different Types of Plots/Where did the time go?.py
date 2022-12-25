6b0f4d32b8397299fdaa44fec69aff7cb5ad1a09
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2022-12-25 14:22:08 +0600

    Update Where did the time go?.py

--- Different Types of Plots/Where did the time go?.py
+++ Different Types of Plots/Where did the time go?.py
@@ -1,5 +1,5 @@
 # Plot the number of hours spent on desk work
-plt.bar(hours.desk_work, hours.field_work)
+plt.bar(hours.officer, hours.desk_work, label='Desk Work')
 
 # Display the plot
 plt.show()
